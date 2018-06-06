63b59cb1d3c18126cea23ae090e781e26c062009
lepilov_alexander <temnoles666@gmail.com>
2018-06-06 13:31:04 +0300

    add radiobutton on js lesson

--- text_output_per_page/script.js
+++ text_output_per_page/script.js
@@ -1,18 +1,18 @@
-function out() {
-	var p;
-	p = document.getElementById("out");
-	//p.innerHTML = "Hello";
-	//Дописать информацию
-	//p.innerHTML += " <b>Hello<b/>";
-	//p.innerText += "Hi";
-	//p.insertAdjacentHTML("beforeBegin", "Hi");
-	//beforeBegin - перед открывающим тегом
-	//p.insertAdjacentHTML("afterBegin", "Hello!");
-	//afterBegin - после открывающего тега
-	//p.insertAdjacentHTML("beforeEnd", "Hello!");
-	//beforeEnd - перед закрывающим тегом
-	//p.insertAdjacentHTML("afterEnd", "Hello!");
-	//afterEnd - после закрывающего тега
-	p.outerHTML = "<div class="
-	one ">hello</div>";
+var radio = document.getElementsByName('prim');
+for (var i=0; i<radio.length; i++){
+	radio[i].onchange = testRadio;
+}
+function testRadio() {
+	console.log(this.value);
+}
+document.getElementById('one').onclick = checkRadio;
+
+function checkRadio () {
+	var m = document.getElementsByName('prim');
+	for (var i = 0; i<m.length; i++){
+		if (m[i].checked) {
+			alert(m[i].value);
+			break;
+		}
+	}
 }
