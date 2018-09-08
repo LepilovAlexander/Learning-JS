0dd80abeceda903f53f8980b4387951330af94fc
lepilov_alexander <temnoles666@gmail.com>
2018-09-08 13:41:18 +0300

    Fixi let lesson

--- ES6/let/let.js
+++ ES6/let/let.js
@@ -1,13 +1,9 @@
-"use strict"
-
 var buttons = document.querySelectorAll('button');
-
-var i;
 
 for (let i = 0; i < buttons.length; i++) {
   var button = buttons[i];
   button.innerText = i;
-  button.onclick = function (e) {
+  button.onclick = function () {
     console.log(i);
 
   };
