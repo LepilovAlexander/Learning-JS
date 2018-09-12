5c98e1f43373bb5cb555589b898ffd8c90498d38
lepilov_alexander <temnoles666@gmail.com>
2018-09-12 16:43:40 +0300

    add lessons: objects and for...of

---
+++ ES6/for...of/script.js
@@ -0,0 +1,13 @@
+let browsers = ['Chrome', 'Firefox', 'Edge', 'Safari', 'Opera'];
+
+// for (let browser in browsers ) {
+//   console.log(browser);
+// }
+// for (let index in browsers) {
+//   console.log(browsers[index]);
+// }
+for (let index of browsers) {
+  console.log(browsers[index]);
+}
+
+console.log(browsers);
