4777dbf9481dd04de2c2da50aaf474ae2c1bd171
Lepilov Alexander <temnoles666@gmail.com>
2018-09-30 23:11:34 +0300

    1

--- photo_effect/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('myslide').onmousemove = function (event){
-	var x = event.offsetX;//относительно родителя!
-	console.log(x);
-	document.getElementById('two').style.width = x + 'px';
-}
-
-document.getElementById('myslide').onmouseleave = function(event){
-	document.getElementById('two').style.width = '375px';
-}
--- window after 5 sec/script.js
+++
@@ -1,6 +0,0 @@
-var delay_popup = 1000;
-setTimeout("document.getElementById('bg_popup').style.display='block'", delay_popup);
-document.getElementById("closeModal").onclick = closeModalWindow;
-function closeModalWindow () {
-	document.getElementById('bg_popup').style.display='none'; return false;
-}
